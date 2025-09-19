53064c28e28bfaf7c378a2e40af4cf20a9d4d380
Wajiha Malik <aleenajabeen19648@gmail.com>
2025-09-19 20:50:37 +0500

    ExpertGuide added

--- src/components/ExpertGuide.jsx
+++ src/components/ExpertGuide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ExpertGuide() {
   return (
-    <div className='font-[Inter] w-[85%] lg:w-[66%] py-4 px-2 mx-auto'>
+    <div className='font-[Inter] bg-white w-[85%] lg:w-[66%] py-4 px-2 mx-auto'>
         <h2 className='font-bold text-[#575757] text-2xl lg:text-[45px]'>Want ExpertGuidance? </h2>
         <p className='text-[#656565] text-base lg:text-[21px]'>Fill out the form and our expert will contact u ?</p>
         <form className='w-full lg:w-[70%] mx-auto my-4 flex flex-col gap-2 my-2 py-8 px-2 shadow-md shadow-[#00000040] rounded-2xl'>
