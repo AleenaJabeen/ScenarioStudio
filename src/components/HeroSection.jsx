5d75ef648ee697a4a1e4367e41631a9ccdd784bc
Wajiha Malik <aleenajabeen19648@gmail.com>
2025-09-19 19:15:02 +0500

    Herosection added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 function HeroSection() {
   return (
     <>
-    <div>
-      
+    <div className='w-[66%] mx-auto mt-[93px] py-6'>
+      <h2 className='text-[#0C0C0C] font-bold text-[45px] text-center'>Scenario Studio</h2>
+      <p className='text-[#656565] text-[22px] text-center'>Explore  your mortgage possibilities with our Scenario Studio . Input your details to simulate 
+different mortgage  scenario and gain insights into your borrowing  potential.</p>
+
     </div>
     </>
   )
