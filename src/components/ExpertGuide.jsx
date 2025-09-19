c06b2869316ae0b9759bd9c8d8090da0811be26e
Wajiha Malik <aleenajabeen19648@gmail.com>
2025-09-19 21:01:02 +0500

    bg issue resolved

--- src/components/ExpertGuide.jsx
+++ src/components/ExpertGuide.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function ExpertGuide() {
   return (
-    <div className='font-[Inter] bg-white w-[85%] lg:w-[66%] py-4 px-2 mx-auto'>
+    <div className='font-[Inter]  w-[85%] lg:w-[66%] py-4 px-2 mx-auto'>
         <h2 className='font-bold text-[#575757] text-2xl lg:text-[45px]'>Want ExpertGuidance? </h2>
         <p className='text-[#656565] text-base lg:text-[21px]'>Fill out the form and our expert will contact u ?</p>
-        <form className='w-full lg:w-[70%] mx-auto my-4 flex flex-col gap-2 my-2 py-8 px-2 shadow-md shadow-[#00000040] rounded-2xl'>
+        <form className='w-full bg-white lg:w-[70%] mx-auto my-4 flex flex-col gap-2 my-2 py-8 px-2 shadow-md shadow-[#00000040] rounded-2xl'>
             <div className="w-full lg:w-[80%] mx-auto flex flex-col gap-2 ">
           <label htmlFor="property" className="text-[10px] text-[#FFAD01]">
           First Name
